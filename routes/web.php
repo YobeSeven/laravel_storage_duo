760690feddb70256414ed813c4746b199437198a
Ayoub <elabedayoub1234@gmail.com>
2021-05-03 14:39:02 +0200

    premiere sauvegarde

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\GenreController;
+use App\Http\Controllers\MembreController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::resource('/admin/membre', MembreController::class);
+Route::resource('/admin/genre', GenreController::class);
